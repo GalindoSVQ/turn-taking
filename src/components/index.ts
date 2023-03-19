54cde828ea286dda324ed7cb80a064b0e2516e37
Antonio Galindo <galindosvq@gmail.com>
2023-03-19 21:08:44 +0100

    chore: remove unused files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./Progressbar";
 export * from "./App";
 export * from "./Button";
-export * from "./Button";
 export * from "./List";
 export * from "./Textbox";
 export * from "./Timer";
