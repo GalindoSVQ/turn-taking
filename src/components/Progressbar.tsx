a11e93be5c365093eea3b016dcaa5c634d1afd69
Antonio Galindo <galindosvq@gmail.com>
2023-03-19 21:10:50 +0100

    style(Progressbar): smooth move

--- src/components/Progressbar.tsx
+++ src/components/Progressbar.tsx
@@ -1,12 +1,20 @@
+import { calculateLeftPercentage, getTotalSecondsFromDate } from "src/utils";
+
 type Props = {
-  progress: number;
+  timeRemaining: Date;
+  totalTime: Date;
 };
 
-function Progressbar({ progress }: Props) {
+function Progressbar({ timeRemaining, totalTime }: Props) {
+  const leftSeconds = getTotalSecondsFromDate(timeRemaining);
+  const totalTimeAsSeconds = getTotalSecondsFromDate(totalTime);
+
+  const progress = calculateLeftPercentage(leftSeconds, totalTimeAsSeconds);
+
   return (
     <div class="mt-2 h-2 w-60 bg-bgTint rounded-lg">
       <div
-        class="h-2 bg-primary rounded-lg"
+        class="h-2 bg-primary rounded-lg transition-width transition-slowest ease-in-out max-w-full"
         style={`width: ${progress}%`}
       ></div>
     </div>
